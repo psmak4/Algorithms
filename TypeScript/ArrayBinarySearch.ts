307c2f7c749fa178b7831e8dba2b7cfe5c3b784a
Preston Jones <psmak4@gmail.com>
2023-09-29 23:48:58 -0400

    Create ArrayBinarySearch.ts

---
+++ TypeScript/ArrayBinarySearch.ts
@@ -0,0 +1,15 @@
+export default function ArrayBinarySearch(haystack: number[], needle: number): boolean {
+	let low = 0
+	let high = haystack.length
+
+	do {
+		const middle = Math.floor(low + (high - low) / 2)
+		const value = haystack[middle]
+
+		if (value === needle) return true
+		else if (value > needle) high = middle
+		else low = middle + 1
+	} while (low < high)
+
+	return false
+}
