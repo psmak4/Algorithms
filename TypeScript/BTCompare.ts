a917c2a3a074774e9cf6a9ef066f20289371efec
Preston Jones <psmak4@gmail.com>
2023-09-29 23:18:41 -0400

    Add BTCompare

---
+++ TypeScript/BTCompare.ts
@@ -0,0 +1,15 @@
+type BinaryNode<T> = {
+	value: T
+	left: BinaryNode<T> | null
+	right: BinaryNode<T> | null
+}
+
+export function BTCompare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+	if (a === null && b === null) return true
+
+	if (a === null || b === null) return false
+
+	if (a.value !== b.value) return false
+
+	return BTCompare(a.left, b.left) && BTCompare(a.right, b.right)
+}
